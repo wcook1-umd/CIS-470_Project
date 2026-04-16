52cfe94483f3f65d3d21b1353770c74584dbf724
Ashley Cook <wcook1@umassd.edu>
2026-04-16 14:53:46 -0400

    added a test file

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 function interpret_bf(s) {
-    if (typeof s != "string") { return idk }
+    if (typeof s != "string") { 
+        throw new Error("Argument is not a string.")
+    }
+
+    for (i in s) {
+        console.log(s[i]);
+    }
+    return s;
 }
+
+// module time
+module.exports = interpret_bf;
---
+++ app.test.js
@@ -0,0 +1,7 @@
+const interpret_bf = require('./app.js')
+
+describe('interpret_bf: uh', () => {
+    test('uhhh', () => {
+        expect(interpret_bf("ooh")).toBe("ooh");
+    })
+})
