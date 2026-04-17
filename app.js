96dddd78c0d7b693fce31d74aa3b050fd0016cfc
Ashley Cook <wcook1@umassd.edu>
2026-04-17 05:05:17 -0400

    Finished the BF interpreter and added some tests

--- app.js
+++ app.js
@@ -1,12 +1,81 @@
-function interpret_bf(s) {
-    if (typeof s != "string") { 
+// Brainfuck memory is expected to be able to under- and overflow.
+// Unsigned shift (>>>) interprets numbers as u32s.
+function as_u32(n) {
+    return n >>> 0;
+}
+
+// Interpreter for Brainfuck, a famous esoteric programming language
+function interpret_bf(program, input="") {
+    if (typeof program != "string") { 
         throw new Error("Argument is not a string.")
     }
 
-    for (i in s) {
-        console.log(s[i]);
+    tape = [0];
+    tape_pointer = 0;
+    input_pointer = 0;
+    output = "";
+    loop_stack = [];
+    skip_until_brackets = 0;
+    for(let program_pointer = 0; program_pointer < program.length; program_pointer++) {
+        if (skip_until_brackets <= 0) {
+            switch (program[program_pointer]) {
+                case "+":
+                    tape[tape_pointer] = as_u32(tape[tape_pointer] + 1);
+                    break;
+                case "-":
+                    tape[tape_pointer] = as_u32(tape[tape_pointer] - 1);
+                    break;
+                case ">":
+                    tape_pointer++;
+                    if (tape_pointer >= tape.length) {
+                        tape.append(0)
+                    }
+                    break;
+                case "<":
+                    tape_pointer--;
+                    if (tape_pointer < 0) {
+                        throw new Error("Attempted to move pointer beyond left end of tape");
+                    }
+                    break;
+                case ",":
+                    if (input_pointer >= input.length) {
+                        tape[tape_pointer] = 0; // Return 0 for EOF
+                    } else {
+                        tape[tape_pointer] = as_u32(input.codePointAt(input_pointer));
+                        input_pointer++;
+                    }
+                    break;
+                case ".":
+                    output += String.fromCodePoint(tape[tape_pointer]);
+                    break;
+                case "[":
+                    if (tape[tape_pointer] == 0) {
+                        is_skipping = true;
+                    } else {
+                        loop_stack.append(program_pointer)
+                    }
+                    break;
+                case "]":
+                    if (tape[tape_pointer] != 0) {
+                        program_pointer = loop_stack.at(-1);
+                        loop_stack.pop();
+                    }
+                    break;
+                // everything else is a comment
+            }
+        } else { // a while condition failed; skipping until matching bracket
+            switch (program[program_pointer]) {
+                case "[":
+                    skip_until_brackets++;
+                    break;
+                case "]":
+                    skip_until_brackets--;
+                    break;
+                // skip everything else
+            }
+        }
     }
-    return s;
+    return output;
 }
 
 // module time
--- app.test.js
+++ app.test.js
@@ -1,7 +1,15 @@
 const interpret_bf = require('./app.js')
 
-describe('interpret_bf: uh', () => {
-    test('uhhh', () => {
-        expect(interpret_bf("ooh")).toBe("ooh");
-    })
+describe('interpret_bf', () => {
+    test('Blank program should output nothing', () => {
+        expect(interpret_bf("")).toBe("");
+    });
+
+    test('"<" should crash if moving before the first memory location', () => {
+        expect(() => {interpret_bf("<")}).toThrow("Attempted to move pointer beyond left end of tape");
+    });
+
+    test('",." should output the first character of input', () => {
+        expect(interpret_bf(",.", input="Brainfuck")).toBe("B");
+    });
 })
