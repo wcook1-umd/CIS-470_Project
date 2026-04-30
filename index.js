bddb8e5b0e3380246e1fd74aabc03a515e7f4799
Ashley Cook <wcook1@umassd.edu>
2026-04-30 02:59:57 -0400

    Commented out file-reading functionality
    aws istg

--- index.js
+++ index.js
@@ -3,38 +3,38 @@ const serverless = require('serverless-http');
 const fs = require('node:fs');
 const interpret_bf = require('./bf_interpreter.js')
 
-if (process.argv.length >= 3 && process.argv[1].includes("main.js")) {
-    let fileName = process.argv[2];
+//if (process.argv.length >= 3 && process.argv[1].includes("index.js")) {
+//    let fileName = process.argv[2];
+//
+//    let inp = "";
+//    if (process.argv.length >= 4)
+//        inp = process.argv[3];
+//
+//    let prog = "";
+//    try {
+//        prog = fs.readFileSync(fileName, 'utf8');
+//    } catch (err) {
+//        console.log(process.argv)
+//        console.error(err);
+//    }
+//
+//    console.log(interpret_bf(prog, inp))
+//
+//} else {
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-    let inp = "";
-    if (process.argv.length >= 4)
-        inp = process.argv[3];
+app.get('/default/CIS470-Project', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
 
-    let prog = "";
-    try {
-        prog = fs.readFileSync(fileName, 'utf8');
-    } catch (err) {
-        console.log(process.argv)
-        console.error(err);
-    }
+app.post('/default/CIS470-Project/interpret', (req, res) => {
+    const { code, input } = req.body;
+    const output = interpret_bf(code, input)
+    res.json({ output });
+})
 
-    console.log(interpret_bf(prog, inp))
-
-} else {
-    const app = express();
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.static('public'));
-
-    app.get('/default/CIS470-Project', (req, res) => {
-        res.sendFile(__dirname + '/public/index.html');
-    })
-
-    app.post('/default/CIS470-Project/interpret', (req, res) => {
-        const { code, input } = req.body;
-        const output = interpret_bf(code, input)
-        res.json({ output });
-    })
-
-    module.exports.handler = serverless(app);
-}
+module.exports.handler = serverless(app);
+//}
