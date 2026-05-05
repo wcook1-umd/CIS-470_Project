f9b9a719547bf65a64a9359891417a170a514aa0
Ashley Cook <wcook1@umassd.edu>
2026-05-05 00:39:04 -0400

    Fixed import in index.js
    i had to export as_u8() to do tests on it but it fricked with all my other imports of the file >:/

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const fs = require('node:fs');
-const interpret_bf = require('./src/bf_interpreter.js')
+const interpreter = require('./src/bf_interpreter.js')
+const interpret_bf = interpreter.interpret_bf
 
 if (process.argv.length >= 3 && process.argv[1].includes("index.js")) {
     let fileName = process.argv[2];
